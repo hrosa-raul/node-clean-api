bd1572f44f28b44df6f294814b62306cc428d690
Raul Rosa <raul.hrosa@gmail.com>
2022-01-05 16:14:42 -0300

    refactor: move all signup protocols to a single file

---
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../protocols'
+export * from '../../protocols/email_validator'
+export * from '../../../domain/usecases/add-account'
+export * from '../../../domain/models/account'
