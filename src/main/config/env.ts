50be794bdea0b0675308eab568c8a42ae4439b75
Raul Rosa <raul.hrosa@gmail.com>
2022-02-06 15:55:31 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb+srv://raulhrosa:gfLCDp7gqB!i@cluster0.q5jqi.mongodb.net/clean_node_api?retryWrites=true&w=majority',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || '12sds==SD'
 }
